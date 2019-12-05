f3c460b8b0b93254419c68df4cec4e97321a1857
João Melo <jopcmelo@gmail.com>
2019-12-05 08:41:01 -0300

    chore: Add global styles

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export default {
+  white: '#fff',
+  grey: '#ddd',
+  primary: '#ee4d64',
+  secondary: '#f5f5f5',
+  danger: '#de3b3b',
+  info: '#4d85ee',
+  dark: '#444',
+  black: '#000',
+  darkGrey: '#999',
+};
---
+++ src/styles/global.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+import colors from './colors';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  *:focus {
+    outline: 0;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+    background: ${colors.secondary};
+  }
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+  h1, h2, h3, h4, h5, h6, th {
+    color: ${colors.dark};
+  }
+  p, span {
+    color: ${colors.darkGrey};
+  }`;
